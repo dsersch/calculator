66bec9a28bf1255f87d077fb8f3ddf1646a6f2fb
David Sersch <dsersch640@gmail.com>
2017-08-30 08:46:14 -0700

    stack operators

--- js/main.js
+++ js/main.js
@@ -2,32 +2,70 @@ var readout = document.querySelector('#readout');
 var digits = document.querySelectorAll('.digit');
 var operators = document.querySelectorAll('.operator');
 var equalsBtn = document.querySelector('#equals');
-var value1;
-var value2;
-var op;
+var clearBtn = document.querySelector('#clear');
+var allClearBtn = document.querySelector('#all-clear')
+var firstNumber;
+var secondNumber = Number(readout.innerText);
+var operation;
+var isAnAnswer = undefined;
 
 digits.forEach(function(btn){
     btn.addEventListener('click', function (){
-        console.log(this.innerText);
-        //"this" is the button being clicked...
+        if (isAnAnswer === true) {
+            allClear();
+            readout.innerText += this.innerText;
+        } else {
         readout.innerText += this.innerText;
+        }
     })
 })
+
 
 operators.forEach(function(btn){
     btn.addEventListener('click', function(){
-        console.log(this.innerText)
-        readout.innerText += this.innerText;
-        value1 = Number(readout.innerText);
-        readout.innerText = '';
-        op = this.operator;
-    })
-})
+        // if (firstNumber === undefined){
+            firstNumber = Number(readout.innerText);
+            operation = this.innerText;
+            readout.innerText = '';
+        // } else {
+        //     secondNumber = Number(readout.innerText);
+        //     firstNumber = evaluate(operation, firstNumber, secondNumber);
+        //     console.log(firstNumber);
+        //     operation = this.innerText;
+        //     readout.innerText = '';
+        // }  
+        })
 
-function solve(){
-    value2 = Number(readout.innerText);
+function allClear() {
+    firstNumber = undefined;
+    secondNumber = undefined;
+    operator = undefined;
+    isAnAnswer = false;
+    readout.innerText = '';
+}
+function clear(){
+    readout.innerText = '';
+}
+clearBtn.addEventListener('click', clear);
+allClearBtn.addEventListener('click', allClear);
+
+function evaluate(operation, num1, num2){
+    switch(operation) {
+        case '+':
+            return firstNumber + secondNumber
+        case '-':
+            return firstNumber - secondNumber
+        case '*': 
+            return firstNumber * secondNumber
+        case '/':
+            return firstNumber / secondNumber
+    }
 }
 
 equalsBtn.addEventListener('click', function(){
-    
+    secondNumber = Number(readout.innerText);
+    // debugger
+    firstNumber = evaluate(operation, firstNumber, secondNumber);
+    readout.innerText = firstNumber;
+    isAnAnswer = true;
 })
